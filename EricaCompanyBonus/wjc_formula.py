9ff504489096d13501639fcb5b019c4bb674e229
hms5232 <hms5232@yahoo.com.tw>
2020-01-22 12:22:08 +0800

    更新成可由使用者自行指定要算第幾行的總和
    順便補上註解及改成可以被 import 的樣子

--- EricaCompanyBonus/wjc_formula.py
+++ EricaCompanyBonus/wjc_formula.py
@@ -1,10 +1,17 @@
-def count_row_num(n):
-	if n == 10:
+# 算出該行某一列的數字
+def count_row_num(n, c):
+	if n == c:
 		return 2**n - 1
-	return count_row_num(n+1) - 2**(10-(n+1))
+	return count_row_num(n+1, c) - 2**(c-(n+1))
 
-n = 10
-ans = 0
-for i in range(1, n+1):
-	ans += count_row_num(i)
-print(ans)
+
+def main():
+	n = int(input())  # 第幾行
+	ans = 0
+	for i in range(1, n+1):
+		ans += count_row_num(i, n)
+	print(ans)
+
+
+if __name__ == '__main__':
+	main()
